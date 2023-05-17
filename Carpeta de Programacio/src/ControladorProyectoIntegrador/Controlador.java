2acec97cf3a17245c774d180b4827acb0a123380
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2023-05-17 11:36:15 +0200

    Update Controlador.java
    Actualización Controlador.java

--- Carpeta de Programacio/src/ControladorProyectoIntegrador/Controlador.java
+++ Carpeta de Programacio/src/ControladorProyectoIntegrador/Controlador.java
@@ -1,11 +1,14 @@
 /**
- * 
+ * Este es el paquete que contiene la clase Controlador para el Proyecto Integrador.
  */
 package ControladorProyectoIntegrador;
 
 /**
+ * Clase Controlador.
+ * Esta clase es parte del modelo de controlador en la estructura MVC del Proyecto Integrador.
+ * Su objetivo es manejar la lógica de la aplicación y dirigir el flujo de datos entre la Vista y el Modelo.
+ * 
  * @author Mario
- *
  */
 public class Controlador {
 
