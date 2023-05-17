0f7bc7b12e1b0dcb7d3ff34d4687315c4552b318
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2023-05-17 11:40:07 +0200

    Update Modelo.java
    Actualización Modelo.java

--- Carpeta de Programacio/src/ModeloProyectorIntegrador/Modelo.java
+++ Carpeta de Programacio/src/ModeloProyectorIntegrador/Modelo.java
@@ -1,11 +1,15 @@
 /**
- * 
+ * Este es el paquete que contiene la clase Modelo para el Proyecto Integrador.
  */
 package ModeloProyectorIntegrador;
 
 /**
+ * Clase Modelo.
+ * Esta clase es parte del modelo en la estructura MVC del Proyecto Integrador.
+ * Su objetivo es manejar la lógica de negocios y las operaciones de datos de la aplicación.
+ * Aquí es donde se implementarán las interacciones con la base de datos y se manipularán los datos.
+ * 
  * @author Mario
- *
  */
 public class Modelo {
 
