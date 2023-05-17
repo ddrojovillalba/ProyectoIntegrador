c078cbf75bc68812e2356c99aeba90b08162a3ac
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2023-05-17 11:46:07 +0200

    Update DetallesAlumnos.java
    Actualización DetallesAlumnos.java

--- Carpeta de Programacio/src/VentanaProyectoIntegrador/DetallesAlumnos.java
+++ Carpeta de Programacio/src/VentanaProyectoIntegrador/DetallesAlumnos.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Este paquete contiene las clases de la interfaz de usuario para el Proyecto Integrador.
  */
 package VentanaProyectoIntegrador;
 
@@ -7,19 +7,33 @@ import javax.swing.*;
 import java.awt.Font;
 
 /**
+ * Clase DetallesAlumnos.
+ * Esta clase es una ventana de la interfaz de usuario que muestra los detalles de los alumnos.
+ * Hereda de JFrame y se utiliza para presentar información detallada sobre un alumno específico.
+ * 
  * @author Mario
- *
  */
 public class DetallesAlumnos extends JFrame {
 
+	// Etiqueta para el título de la ventana
 	JLabel lblDetalles;
+	
+	// Etiqueta para mostrar los datos del alumno
 	JLabel lblDatos;
 	
+	/**
+	 * Constructor de la clase DetallesAlumnos.
+	 * Inicializa la ventana y los componentes de la interfaz de usuario.
+	 */
 	public DetallesAlumnos() {
 		super("Detalles de alumnos");
 		inicializarComponentes();
 	}
 
+	/**
+	 * Método para inicializar los componentes de la interfaz de usuario de la ventana.
+	 * Configura el layout, crea e inicializa las etiquetas, y las añade al contenedor principal.
+	 */
 	private void inicializarComponentes() {
 		getContentPane().setLayout(null);
 		
@@ -31,6 +45,5 @@ public class DetallesAlumnos extends JFrame {
 		lblDatos = new JLabel("");
 		lblDatos.setBounds(28, 97, 385, 156);
 		getContentPane().add(lblDatos);
-		
 	}
 }
