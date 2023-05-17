633bf331872e3a2dd394228d93a8d5c3cc03e0c7
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2023-05-17 11:38:19 +0200

    Update Main.java
    Actualización Main.java

--- Carpeta de Programacio/src/Main/Main.java
+++ Carpeta de Programacio/src/Main/Main.java
@@ -1,20 +1,26 @@
 /**
- * 
+ * Este es el paquete principal (Main) que contiene la clase Main para iniciar la ejecución de la aplicación.
  */
 package Main;
 
 /**
+ * Clase Main.
+ * Esta clase contiene el método principal (main) que es el punto de entrada para cualquier aplicación en Java.
+ * Aquí es donde comienza la ejecución del programa.
+ * 
  * @author Mario
- *
  */
 public class Main {
 
 	/**
-	 * @param args
+	 * El método main.
+	 * Este método se llama automáticamente por la JVM cuando se inicia el programa.
+	 * 
+	 * @param args Argumentos de la línea de comandos, que son una serie de Strings pasados al programa 
+	 *             cuando se ejecuta desde la consola.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Aquí es donde debes poner el código para iniciar y controlar tu aplicación.
 	}
 
 }
