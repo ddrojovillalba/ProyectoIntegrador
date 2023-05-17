7ca39266386bc6bbf4376b98db30af7b99bb9e75
David Rojo Villalba <118904055+ddrojovillalba@users.noreply.github.com>
2023-05-17 11:44:01 +0200

    Update DetalleProyectos.java
    Actualización DetalleProyectos.java

--- Carpeta de Programacio/src/VentanaProyectoIntegrador/DetalleProyectos.java
+++ Carpeta de Programacio/src/VentanaProyectoIntegrador/DetalleProyectos.java
@@ -1,28 +1,42 @@
 /**
- * 
+ * Este paquete contiene las clases de la interfaz de usuario para el Proyecto Integrador.
  */
 package VentanaProyectoIntegrador;
 
 import java.awt.Font;
-
 import javax.swing.*;
 
 /**
+ * Clase DetalleProyectos.
+ * Esta clase es una ventana que muestra los detalles específicos de los proyectos.
+ * Hereda de JFrame y se utiliza para presentar información detallada sobre un proyecto específico.
+ * 
  * @author Mario
- *
  */
 public class DetalleProyectos extends JFrame {
 	
+	// Etiqueta para el título de la ventana
 	JLabel lblDetalles;
+	
+	// Etiqueta para mostrar los datos del proyecto
 	JLabel lblDatos;
 	
+	/**
+	 * Constructor de la clase DetalleProyectos.
+	 * Inicializa la ventana y los componentes de la interfaz de usuario.
+	 */
 	public DetalleProyectos() {
 		super("Detalles de Proyectos");
 		inicializarComponentes();
 	}
 
+	/**
+	 * Método para inicializar los componentes de la interfaz de usuario de la ventana.
+	 * Configura el layout, crea e inicializa las etiquetas, y las añade al contenedor principal.
+	 */
 	private void inicializarComponentes() {
 		getContentPane().setLayout(null);
+		
 		lblDetalles = new JLabel("Detalle Proyectos");
 		lblDetalles.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		lblDetalles.setBounds(28, 36, 192, 32);
@@ -31,6 +45,5 @@ public class DetalleProyectos extends JFrame {
 		lblDatos = new JLabel("");
 		lblDatos.setBounds(28, 97, 385, 156);
 		getContentPane().add(lblDatos);
-		
 	}
 }
